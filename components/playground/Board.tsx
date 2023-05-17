af26f5ac9e3abe707c170c0f6753216508981c49
minlost <97989349+minlost@users.noreply.github.com>
2023-05-17 20:29:47 +0200

    closing cow func

--- components/playground/Board.tsx
+++ components/playground/Board.tsx
@@ -6,13 +6,19 @@ import { useGlobalContext } from "@/context/GlobalContext"
 
 const Board = () => {
   const { flexValue, cowStore, setOpenedCow } = useGlobalContext()
-
   const { justifyContent, alignItems, flexDirection, gap, flexWrap } = flexValue
+
+  const closeCow = () => {
+    if (cowStore.length < 0) {
+      return
+    }
+    if (cowStore.length > 0) {
+      setOpenedCow(null)
+    }
+  }
+
   return (
-    <div
-      className="flex justify-center text-center "
-      onClick={() => setOpenedCow(null)}
-    >
+    <div className="flex justify-center text-center " onClick={closeCow}>
       <div
         className={`bg-primary w-[300px] h-[300px]  border-b border-l border-r rounded-b-lg md:border-t md:rounded-t-lg flex p-2 `}
         style={{
